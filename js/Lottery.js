8122f8ae9c9bab6ea33da28a1750448e3d95682e
hongqx <hongqiongxing@163.com>
2015-09-08 19:48:24 +0800

    增加逻辑

--- js/Lottery.js
+++ js/Lottery.js
@@ -4,9 +4,11 @@
         colors:["#bef1f4","#85dee4","#60d0d8"],//大转盘奖品区块对应背景颜色
         angle:0,//变形的角度
         rotateDeg:0,//旋转的角度，通过计算获取
-        startDeg:0,//转盘绘制开始的角度
-        containerId:null,
-        bRotate:false
+        startDeg:-10,//转盘绘制开始的角度
+        backDeg:-60,//内部元素回转角度，该角度需要按照实际需求调试指定
+        containerId:null,//转盘选项容器id
+        rotateDomId:null,//转动的domid
+        bRotate:false//是否正在转动标志位
       },
       data:{},//需要展现的数据
       initOptions:function(options,data){
@@ -47,104 +49,210 @@
               this.container.append(_div);
           }
       },
-      rotateFn : function (total,item,txt,fn){
+      /*转盘转动逻辑*/
+      rotateFn : function (item,txt,fn){
           this.endFn =  fn;
-          var angles = item * (360 / total) - (360 / (total*2))+270;
+          var angles = 90- item * this.config.rotateDeg - this.config.startDeg + this.config.rotateDeg / 2;
+          if(angles < 0){
+            angles += 360;
+          }
           console.info("转动的角度为："+angles);
-         /* if(angles<270){
-            angles = 270 - angles; 
-          }else{
-            angles = 360 - angles + 270;
-          }*/
-          this.container.stopRotate();
+          $("#"+this.config.rotateDomId).stopRotate();
           var _this =this;
-          this.container.rotate({
+          _this.config.bRotate = false;
+          $("#"+this.config.rotateDomId).rotate({
             angle:0,
-            animateTo:angles,
+            animateTo:angles+360*10,
             duration:1000,
             callback:function (){
               _this.config.bRotate = !_this.config.bRotate;
               ////////////////
-              _this.endFn? _this.endFn(txt):alert(txt);       
+              _this.endFn ? _this.endFn(txt) : alert(txt);       
               ////////////////
             }
           });
         }
    };
-   var data = {
-        "lottery_id": 132,
-        "create_id": "xujiahui",
-        "create_source": 0,
-        "update_id": "xujiahui",
-        "update_source": 0,
-        "summary": "",
-        "title": "小米应用商店抽奖（张哲）",
-        "callback": "",
-        "luck_draw_obj": 1,
-        "one_day_win_num": 99,
-        "one_cycle_win_num": 99,
-        "one_day_draw_num": 99,
-        "lottery_icon": "http://r4.ykimg.com/0510000055DACE576714C06D3B09716F",
-        "layer_top_img": "",
-        "layer_left_img": "",
-        "layer_center_img": "",
-        "act_copy": "",
-        "status": 1,
-        "risk_control_switch": 0,
-        "defense_switch": 0,
-        "effective_type": 0,
-        "begintime": "2015-08-24 16:40:00",
-        "expiretime": "2015-09-04 23:55:00",
-        "_inserttime": "2015-08-24 15:57:27",
-        "_updatetime": "2015-08-26 11:18:29",
-        "prizes": [
-        {
-        "prize_id": "581",
-        "prize_no": "1",
-        "prize_level": "一等奖",
-        "prize_name": "小米电视55寸",
-        "prize_copy": "恭喜您获得小米电视55寸1台！",
-        "prize_icon": "http://r3.ykimg.com/0510000055DD303A6714C05A8A014061"
+   var Lottery = {
+        //奖品信息
+        lotteryInfo:{
+              "lottery_id": 133,
+              "title": "小米应用商店抽奖（正式）",
+              "lottery_icon": "http://r4.ykimg.com/0510000055DD7A6E6714C02FD407FC14",
+              "begintime": "2015-08-28 00:00:00",
+              "expiretime": "2015-09-05 00:00:00",
+              "_inserttime": "2015-08-26 16:36:13",
+              "_updatetime": "2015-08-27 14:36:49",
+              "prizes": [
+                  {
+                      "prize_id": "604",
+                      "prize_no": "1",
+                      "prize_level": "一等奖",
+                      "prize_name": "小米55寸电视",
+                      "prize_copy": "恭喜您获得小米55寸电视1台！",
+                      "prize_icon": "http://r1.ykimg.com/0510000055DD82006714C00F66069508",
+                      "message_content": "【中奖通知】恭喜亲获得小米55寸电视1台，如您已经将收货信息（包括姓名、地址、手机号码）填写完成，我们会在活动结束后10个工作日内与您联系并寄出奖品，若未按提示填写收货信息或信息填写有误将均视为弃奖，感谢您的参与。",
+                      "user_type": "0",
+                      "remain_num": 2
+                  },
+                  {
+                      "prize_id": "605",
+                      "prize_no": "2",
+                      "prize_level": "二等奖",
+                      "prize_name": "Xbox One体感游戏机",
+                      "prize_copy": "恭喜您获得Xbox One体感游戏机1台！",
+                      "prize_icon": "http://r3.ykimg.com/0510000055DD30466714C07F120ED0AE",
+                      "message_content": "【中奖通知】恭喜亲获得Xbox One体感游戏机1台，如您已经将收货信息（包括姓名、地址、手机号码）填写完成，我们会在活动结束后10个工作日内与您联系并寄出奖品，若未按提示填写收货信息或信息填写有误将均视为弃奖，感谢您的参与。",
+                      "user_type": "0",
+                      "remain_num": 2
+                  },
+                  {
+                      "prize_id": "606",
+                      "prize_no": "3",
+                      "prize_level": "三等奖",
+                      "prize_name": "小米4手机",
+                      "prize_copy": "恭喜您获得小米4手机1台！",
+                      "prize_icon": "http://r2.ykimg.com/0510000055DD82756714C076940163E0",
+                      "message_content": "【中奖通知】恭喜亲获得小米4手机1台，如您已经将收货信息（包括姓名、地址、手机号码）填写完成，我们会在活动结束后10个工作日内与您联系并寄出奖品，若未按提示填写收货信息或信息填写有误将均视为弃奖，感谢您的参与。",
+                      "user_type": "0",
+                      "remain_num": 6
+                  },
+                  {
+                      "prize_id": "611",
+                      "prize_no": "4",
+                      "prize_level": "四等奖",
+                      "prize_name": "优酷会员周卡",
+                      "prize_copy": "恭喜您获得优酷会员周卡1张！",
+                      "prize_icon": "http://r3.ykimg.com/0510000055DEEA5E6714C02A7000D088",
+                      "message_content": "【中奖通知】恭喜您获得优酷会员周卡1张。会员周卡使用方法：1、登录http://vip.youku.com/；2、点击”我的账户“-会员卡激活 输入密码和验证码即可。",
+                      "user_type": "0",
+                      "remain_num": 48
+                  },
+                  {
+                      "prize_id": "612",
+                      "prize_no": "99",
+                      "prize_level": "谢谢参与",
+                      "prize_name": "谢谢参与",
+                      "prize_copy": "谢谢参与",
+                      "prize_icon": "http://r1.ykimg.com/0510000055DEEA806714C073A0008879",
+                      "message_content": "",
+                      "user_type": "0",
+                      "remain_num": 72442
+                  }
+              ],
+              "tasks": []
+          },
+        //抽奖参数配置
+        lotteryConfig:{
+            id:133,//抽奖id
+            sourceUrl:"http://v.youku.com/x_mLotteryInfo",//奖品信息获取接口
+            actUrl:"http://v.youku.com/x_mLotteryDraw"//抽奖接口
         },
-        {
-        "prize_id": "582",
-        "prize_no": "2",
-        "prize_level": "二等奖",
-        "prize_name": "Xbox One体感游戏机",
-        "prize_copy": "恭喜您获得Xbox One体感游戏机1部！",
-        "prize_icon": "http://r3.ykimg.com/0510000055DD30466714C07F120ED0AE"
+        loseNum:1,//为中奖信息显示模块数
+        loseConfig:{
+
         },
-        {
-        "prize_id": "583",
-        "prize_no": "3",
-        "prize_level": "三等奖",
-        "prize_name": "小米4手机",
-        "prize_copy": "恭喜您获得小米4手机1部！",
-        "prize_icon": "http://r1.ykimg.com/0510000055DD30586714C067F20D2F52"
+        prizeInfo:{},//抽奖结果
+        flag : true,//用于给抽奖按钮加锁
+        ifContinue:true,//用于标识是否可以继续抽奖
+        init : function(){
+            var _width = $(".turntable").width();
+            $(".turntable").height(_width);
+            this.rotate = Rotate;
+            this.initLottery();
+            this.tipContainer = $(".tip-content");
         },
-        {
-        "prize_id": "596",
-        "prize_no": "4",
-        "prize_level": "四等奖",
-        "prize_name": "优酷会员周卡",
-        "prize_copy": "恭喜您获得优酷会员周卡1张！",
-        "prize_icon": "http://r4.ykimg.com/0510000055DD30BF6714C0660603E3FC"
+        //初始化抽奖信息
+        initLottery : function(){
+          /*初始化大转盘*/
+          this.rotate.initOptions({containerId:"js_container",rotateDomId:"js_rotate"},this.lotteryInfo.prizes);
+          this.rotate.drawRotate();
+          //初始化时间信息
+          var beginTime = this.lotteryInfo.begintime.split(" ")[0].split("-").join(".");
+          var deadLineTime = this.lotteryInfo.expiretime.split(" ")[0].split("-").join(".");
+          $("#js_dateLine").html("活动日期："+beginTime+"-"+deadLineTime);
+          var _result = this.checkTime(this.lotteryInfo.begintime,this.lotteryInfo.expiretime);
+          if(_result === 0){
+             var _this =  this;
+             $('#js_pointer').on("click",function(){
+                if(_this.ifContinue && _this.flag){
+                   _this.flag = false;
+                   _this.checkIfLogin(_this.getLotteryInfo);
+                }else{
+                    _this.showResultMessage(this.error_msg,1);
+                    return;
+                } 
+             });
+          }else if(_result===-1){
+              alert("活动还未开始，请您稍后再参加！");
+              $('#js_pointer').on("click",function(){
+                 alert("活动还未开始，请您稍后再参加！");
+              });
+          }else{
+              alert("活动已经结束,期待您的下次参与！");
+              $('#js_pointer').on("click",function(){
+                 alert("活动已经结束，期待您的下次参与！");
+              });
+          }
         },
-        {
-        "prize_id": "597",
-        "prize_no": "99",
-        "prize_level": "谢谢参与",
-        "prize_name": "谢谢参与！",
-        "prize_copy": "谢谢参与！",
-        "prize_icon": "http://r4.ykimg.com/0510000055DD30EB6714C00B6F0DECBB"
-        }
-        ],
-        "tasks": []
-        };
-        win.onload = function(){
-          win.Rotate = Rotate;
-           Rotate.initOptions({containerId:"js_rotate"},data.prizes);
-           Rotate.drawRotate();
-        }
+        /*信息获取接口*/
+        getData : function(url,data,fn){
+           $.ajax({
+            url:url,
+            type:"get",
+            data:data,
+            dataType:"jsonp",
+            crossDomain:"true",
+            success:function(data){
+              fn(data);
+            }
+           });
+        },
+        checkIfLogin : function(fn){
+          var _this =  this;
+          if(!islogin()){
+            $(document).bind("userlogin",function(){
+                fn();
+            });
+            login();
+          }else{
+             fn();
+          }
+        },
+        getLotteryInfo : function(){
+            var _this = this;
+            this.getData(this.lotteryConfig.sourceUrl,{"id":this.lotteryConfig.id},function(data){
+               if(data && data.error_code === 0){
+                 _this.lotteryInfo = data.result;
+                 _this.initLottery();
+               }else{
+                 alert("获取信息失败，请重试!");
+               }
+            });
+        },
+        showResultMessage:function(){
+
+        },
+        //检查是否过期
+        checkTime:function(sDate,dDate){
+               var nowTime = new Date().getTime();
+               var sTime = new Date(sDate.split("-").join("/")).getTime();
+               var dTime = new Date(dDate.split("-").join("/")).getTime();
+               if(nowTime >= sTime && nowTime <= dTime){
+                 return 0;
+               }else if(nowTime <sTime){
+                 return -1;
+               }else{
+                  return -2;
+               }
+        },
+   };
+   win.Lottery = Lottery;
+    win.onload = function(){
+      /*win.Rotate = Rotate;
+       Rotate.initOptions({containerId:"js_container",rotateDomId:"js_rotate"},data.prizes);
+       Rotate.drawRotate();*/
+       Lottery.init();
+    };
        
 })(window,jQuery);
